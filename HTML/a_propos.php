441d3c096141ae52f71ebe2adcdcc209a87e7aa8
farming19mods31 <93335715+farming19mods31@users.noreply.github.com>
2022-01-07 21:59:21 +0100

    Add files via upload

---
+++ HTML/a_propos.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<?php include '../PHP/page_template.php'?>
+	<title>ESSENTIELRP website | A propos</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="./CSS/styles3.css">
+</head>
+<body>
+	<?php include '../PHP/footer_template.php'?>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<?php include './PHP/page_template.php'?>
+	<title>ESSENTIELRP website | Accueil</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="./CSS/styles1.css">
+</head>
+<body>
+
+
+	<br><br><br><br>
+
+	<a href="https://discord.com/oauth2/authorize?client_id=901732177602031616&scope=bot&permissions=805314622" id="majouter">
+		Ajouter à votre serveur
+	</a>	
+
+	<br><br><br>
+
+
+	<a href="https://discord.gg/xgVhYvU5hX" id="support">
+		Rejoindre le support
+	</a>
+
+	<?php include './PHP/footer_template.php'?>
+
+</body>
+</html>
