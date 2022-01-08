c5c6a8beb53cc03c3b594d44217cd829094c54fc
farming19mods31 <93335715+farming19mods31@users.noreply.github.com>
2022-01-08 18:57:33 +0100

    Add files via upload

---
+++ ESSENTIELRP-website/PHP/page_template.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="../CSS/style_template.css">
+
+	  <link class="icon" rel="icon" sizes="192x192" href="../Multimedia/Image/ESSENTIELRP_logo.png">
+  	<link rel="shortcut icon" href="../Multimedia/Image/ESSENTIELRP_logo.png" type="image/x-icon">
+  	<link rel="apple-touch-icon" href="../Multimedia/Image/ESSENTIELRP_logo.png" type="image/x-icon">
+</head>
+<body>
+
+	<!-- Haut de page -->
+	<header style="text-align: center;">
+		<img src="../Multimedia/Image/ESSENTIELRP_logo.png" width="80">
+		<h1>
+			ESSENTIELRP website
+		</h1>
+
+		<p>
+			Slogan à venir prochaînement n'hésitez pas à nous en proposer dans <a href="HTML/contact.html">CONTACT</a> !
+		</p>
+	</header>
+
+	<!-- Menu de navigation -->
+	<nav class="menu-nav" style="text-align: center;">
+		<li class="btn">
+			<a href="../index.php">
+				Accueil
+			</a>
+		</li>
+		<li class="btn">
+			<a href="../HTML/a_propos.php">
+				A propos
+			</a>
+		</li>
+		<li class="btn">
+			<a href="../HTML/contact.php">
+				Contact
+			</a>
+		</li>
+	</nav>
+  
+</body>
+</html>
